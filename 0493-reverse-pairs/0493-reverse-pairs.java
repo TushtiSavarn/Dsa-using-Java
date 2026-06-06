2ede5daf4be9610941675a656fe7f2b55c6e3dad
Tushti Savarn <82764533+TushtiSavarn@users.noreply.github.com>
2026-06-06 14:53:01 +0530

    Time: 44 ms (56.93%), Space: 64.4 MB (39.64%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,49 +1,63 @@
 class Solution {
+    int count = 0;
+
     public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1);
-    }
-
-    private int mergeSort(int[] nums, int left, int right) {
-        if (left >= right) return 0;
-
-        int mid = left + (right - left) / 2;
-        int count = 0;
-
-        count += mergeSort(nums, left, mid);
-        count += mergeSort(nums, mid + 1, right);
-        count += countPairs(nums, left, mid, right);
-        merge(nums, left, mid, right);
-
+        mergeSort(nums, 0, nums.length - 1);
         return count;
     }
 
-    private int countPairs(int[] nums, int left, int mid, int right) {
-        int count = 0;
+    void mergeSort(int[] nums, int l, int r) {
+        if (l >= r)
+            return;
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
+
+        merge(nums, l, mid, r);
+    }
+
+    void merge(int[] nums, int l, int mid, int r) {
+
+        // Count reverse pairs
+        int i = l;
         int j = mid + 1;
 
-        for (int i = left; i <= mid; i++) {
-            while (j <= right && (long)nums[i] > 2L * nums[j]) {
+        while (i <= mid && j <= r) {
+            if ((long) nums[i] > 2L * nums[j]) {
+                count += (mid - i + 1);
                 j++;
+            } else {
+                i++;
             }
-            count += (j - (mid + 1));
-        }
-        return count;
-    }
-
-    private void merge(int[] nums, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) temp[k++] = nums[i++];
-            else temp[k++] = nums[j++];
         }
 
-        while (i <= mid) temp[k++] = nums[i++];
-        while (j <= right) temp[k++] = nums[j++];
+        // Merge step
+        int[] temp = new int[r - l + 1];
 
-        for (int p = 0; p < temp.length; p++) {
-            nums[left + p] = temp[p];
+        i = l;
+        j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+
+        while (j <= r) {
+            temp[k++] = nums[j++];
+        }
+
+        for (i = l; i <= r; i++) {
+            nums[i] = temp[i - l];
         }
     }
 }
