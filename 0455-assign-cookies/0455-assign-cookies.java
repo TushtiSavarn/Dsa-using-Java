a0ff4e33552c1712643ece84a87b5acd5b06384a
Tushti Savarn <82764533+TushtiSavarn@users.noreply.github.com>
2026-06-16 12:23:04 +0530

    Time: 15 ms (62.91%), Space: 51.6 MB (36.35%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,31 +1,21 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        // Sort both arrays to use greedy approach
-        // g: greed factors of children
-        // s: sizes of cookies
         Arrays.sort(g);
         Arrays.sort(s);
-      
-        int childCount = g.length;
-        int cookieCount = s.length;
-      
-        int childIndex = 0;
-        int cookieIndex = 0;
-        int satisfiedChildren = 0;
-      
-        // Try to satisfy each child with the smallest possible cookie
-        while (childIndex < childCount && cookieIndex < cookieCount) {
-            // If current cookie can satisfy current child
-            if (s[cookieIndex] >= g[childIndex]) {
-                satisfiedChildren++;
-                childIndex++;
-                cookieIndex++;
+        int child = g.length;
+        int cookie = s.length;
+        int childcount = 0;
+        int cookiecount = 0;
+        int happyChild = 0;
+        while (childcount < child && cookiecount < cookie) {
+            if (s[cookiecount] >= g[childcount]) {
+                happyChild++;
+                childcount++;
+                cookiecount++;
             } else {
-                // Current cookie is too small, try next cookie
-                cookieIndex++;
+                cookiecount++;
             }
         }
-      
-        return satisfiedChildren;
+        return happyChild;
     }
 }
