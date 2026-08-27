7ce938874245b615c229e18d639cca91194bcbeb
Tushti Savarn <82764533+TushtiSavarn@users.noreply.github.com>
2026-08-27 23:13:15 +0530

    Time: 268 ms (26.57%), Space: 46.7 MB (13.55%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        String repeated = "";
+        int count = 0;
+        while (repeated.length() < b.length()) {
+            repeated += a;
+            count++;
+        }
+        if (repeated.contains(b))
+            return count;
+        repeated += a;
+        count++;
+        if (repeated.contains(b))
+            return count;
+        return -1;
+    }
+}
